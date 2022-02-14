42956440c76b2c6dba43f5853371cb0390267e44
ZalgoNoise <zalgo.noise@gmail.com>
2022-02-15 00:33:19 +0100

    new: added placeholder tests for logfile.go

---
+++ log/logfile_test.go
@@ -0,0 +1,27 @@
+package log
+
+import "testing"
+
+func TestNewLogFile(t *testing.T) {}
+
+func TestLogfileMaxSize(t *testing.T) {}
+
+func TestLogfileNew(t *testing.T) {}
+
+func TestLogfileLoad(t *testing.T) {}
+
+func TestLogfileMove(t *testing.T) {}
+
+func TestLogfileHasExt(t *testing.T) {}
+
+func TestLogfileAddExt(t *testing.T) {}
+
+func TestLogfileCutExt(t *testing.T) {}
+
+func TestLogfileSize(t *testing.T) {}
+
+func TestLogfileIsTooHeavy(t *testing.T) {}
+
+func TestLogfileRotate(t *testing.T) {}
+
+func TestLogfileWrite(t *testing.T) {}

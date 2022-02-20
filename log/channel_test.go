3104996c79eaaddc0e5d71566ba378e086c06fed
ZalgoNoise <zalgo.noise@gmail.com>
2022-02-20 10:48:11 +0100

    new: added LogCh multi-entry tests

--- log/channel_test.go
+++ log/channel_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"testing"
 )
 
@@ -290,7 +291,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 			if err := json.Unmarshal(buf.Bytes(), logEntry); err != nil {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- unmarshal error: %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- unmarshal error: %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -301,7 +302,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 			if logEntry.Prefix != test.msg.Prefix {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- prefix mismatch: wanted %s ; got %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- prefix mismatch: wanted %s ; got %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -313,7 +314,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 			if logEntry.Level != test.msg.Level {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- log level mismatch: wanted %s ; got %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- log level mismatch: wanted %s ; got %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -325,7 +326,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 			if logEntry.Msg != test.msg.Msg {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- message mismatch: wanted %s ; got %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- message mismatch: wanted %s ; got %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -337,7 +338,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 			if logEntry.Metadata == nil && test.msg.Metadata != nil {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- retrieved empty metadata object: wanted %s ; got %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- retrieved empty metadata object: wanted %s ; got %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -347,7 +348,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 				return
 			} else if logEntry.Metadata != nil && test.msg.Metadata == nil {
 				t.Errorf(
-					"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- retrieved unexpected metadata object: wanted %s ; got %s",
+					"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- retrieved unexpected metadata object: wanted %s ; got %s",
 					id,
 					bufID,
 					test.msg.Msg,
@@ -361,7 +362,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 				for k, v := range logEntry.Metadata {
 					if v != nil && test.msg.Metadata[k] == nil {
 						t.Errorf(
-							"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- metadata mismatch: key %s contains data ; original message's key %s doesn't",
+							"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- metadata mismatch: key %s contains data ; original message's key %s doesn't",
 							id,
 							bufID,
 							test.msg.Msg,
@@ -374,7 +375,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 
 				if len(logEntry.Metadata) != len(test.msg.Metadata) {
 					t.Errorf(
-						"#%v -- FAILED -- [ChLogger] [Buffer #%v] Log(%s) -- metadata length mismatch -- wanted %v, got %v",
+						"#%v -- FAILED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- metadata length mismatch -- wanted %v, got %v",
 						id,
 						bufID,
 						test.msg.Msg,
@@ -386,7 +387,7 @@ func TestNewLogChMultiLogger(t *testing.T) {
 			}
 
 			t.Logf(
-				"#%v -- PASSED -- [ChLogger] [Buffer #%v] Log(%s) -- %s",
+				"#%v -- PASSED -- [ChLogger] [MultiLogger] [Buffer #%v] Log(%s) -- %s",
 				id,
 				bufID,
 				test.msg.Msg,
@@ -404,6 +405,160 @@ func TestNewLogChMultiLogger(t *testing.T) {
 		go chLogger()
 
 		logCh <- test.msg
+
+	}
+}
+
+func TestNewLogChMultiEntry(t *testing.T) {
+	type log struct {
+		logger LoggerI
+		buf    []*bytes.Buffer
+	}
+	type test struct {
+		log
+		msg []*LogMessage
+		rgx []string
+	}
+
+	var prefix = "multientry-test"
+	var msgs = []string{
+		"test log #0",
+		"test log #1",
+		"test log #2",
+		"test log #3",
+		"test log #4",
+		"test log #5",
+		"test log #6",
+		"test log #7",
+		"test log #8",
+		"test log #9",
+		"test log #10",
+	}
+
+	regxStr := `^\[.*\]\s*\[info\]\s*\[multientry-test\]\s*test log #`
+
+	var regxList = []string{
+		regxStr + `0`,
+		regxStr + `1`,
+		regxStr + `2`,
+		regxStr + `3`,
+		regxStr + `4`,
+		regxStr + `5`,
+		regxStr + `6`,
+		regxStr + `7`,
+		regxStr + `8`,
+		regxStr + `9`,
+		regxStr + `10`,
+	}
+	var maxBufs = 6
+
+	var tests []test
+
+	for a := 1; a <= maxBufs; a++ {
+
+		var bufs []*bytes.Buffer
+		var logs []LoggerI
+		var msgObj []*LogMessage
+		var rxList []string
+
+		for b := 1; b <= a; b++ {
+
+			var w = []io.Writer{}
+			for c := 1; c <= a; c++ {
+				newBuf := &bytes.Buffer{}
+				bufs = append(bufs, newBuf)
+				w = append(w, newBuf)
+			}
+
+			l := New(prefix, TextFormat, w...)
+			logs = append(logs, l)
+		}
+
+		ml := MultiLogger(logs...)
+
+		for d := 0; d < len(msgs); d++ {
+			obj := NewMessage().
+				Prefix(prefix).
+				Message(msgs[d]).
+				Level(LLInfo).
+				Build()
+
+			msgObj = append(msgObj, obj)
+			rxList = append(rxList, regxList[d])
+		}
+
+		obj := test{
+			log: log{
+				logger: ml,
+				buf:    bufs,
+			},
+			msg: msgObj,
+			rgx: regxList,
+		}
+
+		tests = append(tests, obj)
+
+	}
+
+	var verify = func(id int, test test) {
+
+		for bufID, buf := range test.log.buf {
+			if buf.Len() == 0 {
+				t.Logf("Buf #%v has 0-length", bufID)
+			}
+
+			var lines [][]byte
+			var line []byte
+			for _, b := range buf.Bytes() {
+				if b != 10 {
+					line = append(line, b)
+					continue
+				}
+				lines = append(lines, line)
+				line = []byte{}
+			}
+
+			for idx, line := range lines {
+				rgx := regexp.MustCompile(test.rgx[idx])
+
+				if !rgx.MatchString(string(line)) {
+					t.Errorf(
+						"#%v -- FAILED -- [ChLogger] [MultiEntry] [Buffer #%v] Log() x%v -- message race error: messages did not arrive in the right order\n\n%s",
+						id,
+						bufID,
+						len(lines),
+						buf.String(),
+					)
+					return
+				}
+			}
+
+			t.Logf(
+				"#%v -- PASSED -- [ChLogger] [MultiEntry] [Buffer #%v] Log() x%v -- \n\n%s",
+				id,
+				bufID,
+				len(lines),
+				buf.String(),
+			)
+		}
+	}
+
+	for id, test := range tests {
+		defer func() {
+			for _, b := range test.log.buf {
+				b.Reset()
+			}
+		}()
+
+		logCh, chLogger := NewLogCh(test.log.logger)
+
+		go chLogger()
+
+		for _, msg := range test.msg {
+			logCh <- msg
+		}
+
+		verify(id, test)
 
 	}
 }

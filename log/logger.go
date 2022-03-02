c8d90e6a643f15f5d48382ac988d4291280890df
ZalgoNoise <zalgo.noise@gmail.com>
2022-03-02 17:39:51 +0100

    new: added code documentation to logger.go

--- log/logger.go
+++ log/logger.go
@@ -5,6 +5,12 @@ import (
 	"sync"
 )
 
+// LoggerI interface defines the general behavior of a Logger object
+//
+// It lists all the methods that a Logger implements in order to print
+// timestamped messages to an io.Writer, and additional configuration
+// methods to enhance its behavior and application (such as `Prefix()`
+// and `Fields()`; and `SetOuts()` or `AddOuts()`)
 type LoggerI interface {
 	Output(m *LogMessage) error
 	SetOuts(outs ...io.Writer) LoggerI
@@ -49,12 +55,27 @@ type LoggerI interface {
 
 var std = New(defaultConfig)
 
+// LoggerBuilder struct describes a builder object for Loggers
+//
+// The LoggerBuilder object will always be the target for configuration
+// settings that are applied when building a Logger, and only after
+// all elements are set (with defaults or otherwise) it
+// is converted / copied into a Logger
 type LoggerBuilder struct {
 	out    io.Writer
 	prefix string
 	fmt    LogFormatter
 }
 
+// New function allows creating a basic Logger (implementing the LoggerI
+// interface).
+//
+// Its input parameters are a list of objects which implement the
+// LoggerConfig interface. These parameters are iterated through via a
+// `multiConf` object that applies all configurations to the builder.
+//
+// Defaults are automatically applied to all elements which aren't defined
+// in the input configuration.
 func New(confs ...LoggerConfig) LoggerI {
 	builder := &LoggerBuilder{}
 
@@ -79,6 +100,8 @@ func New(confs ...LoggerConfig) LoggerI {
 	}
 }
 
+// Logger struct describes a basic Logger, which is used to print timestamped messages
+// to an io.Writer
 type Logger struct {
 	mu     sync.Mutex
 	out    io.Writer
@@ -88,8 +111,11 @@ type Logger struct {
 	meta   map[string]interface{}
 }
 
-// output setter methods
-
+// SetOuts method will set (replace) the defined io.Writer in the Logger with the list of
+// io.Writer set as `outs`.
+//
+// By default, these input io.Writer will be processed with an io.MultiWriter call to create
+// a wrapper for multiple io.Writers
 func (l *Logger) SetOuts(outs ...io.Writer) LoggerI {
 	l.mu.Lock()
 	defer l.mu.Unlock()
@@ -99,6 +125,11 @@ func (l *Logger) SetOuts(outs ...io.Writer) LoggerI {
 	return l
 }
 
+// AddOuts method will add (append) the list of io.Writer set as `outs` to the defined
+// ioWriter in the logger
+//
+// By default, these input io.Writer will be processed with an io.MultiWriter call to create
+// a wrapper for multiple io.Writers
 func (l *Logger) AddOuts(outs ...io.Writer) LoggerI {
 	l.mu.Lock()
 	defer l.mu.Unlock()
@@ -110,8 +141,13 @@ func (l *Logger) AddOuts(outs ...io.Writer) LoggerI {
 	return l
 }
 
-// prefix setter methods
-
+// Prefix method will set a Logger-scoped (as opposed to message-scoped) prefix string to the logger
+//
+// Logger-scoped prefix strings can be set in order to avoid calling the `MessageBuilder.Prefix()` method
+// repeatedly, and instead doing so via the logger at the beginning of a service or function
+//
+// A logger-scoped prefix is not cleared with new Log messages, but `MessageBuilder.Prefix()` calls will
+// replace it.
 func (l *Logger) Prefix(prefix string) LoggerI {
 	l.mu.Lock()
 	defer l.mu.Unlock()
@@ -121,8 +157,13 @@ func (l *Logger) Prefix(prefix string) LoggerI {
 	return l
 }
 
-// metadata methods
-
+// Fields method will set Logger-scoped (as opposed to message-scoped) metadata fields to the logger
+//
+// Logger-scoped metadata can be set in order to avoid calling the `MessageBuilder.Metadata()` method
+// repeatedly, and instead doing so via the logger at the beginning of a service or function.
+//
+// Logger-scoped metadata fields are not cleared with new log messages, but only added to the existing
+// metadata map. These can be cleared with a call to `Logger.Fields(nil)`
 func (l *Logger) Fields(fields map[string]interface{}) LoggerI {
 	l.mu.Lock()
 	defer l.mu.Unlock()

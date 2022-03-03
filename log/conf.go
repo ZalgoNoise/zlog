8921ff9cb626a04cc8e0dc63ad9e3a33459ff64d
ZalgoNoise <zalgo.noise@gmail.com>
2022-03-03 13:19:08 +0100

    new: added code documentation for conf.go

--- log/conf.go
+++ log/conf.go
@@ -5,14 +5,25 @@ import (
 	"os"
 )
 
+// LoggerConfig interface describes the behavior that a LoggerConfig object should have
+//
+// The single Apply(lb *LoggerBuilder) method allows for different modules to apply changes to a
+// LoggerBuilder, in a non-blocking way for other features.
+//
+// Each feature should implement its own structs with their own methods; where they can implement
+// Apply(lb *LoggerBuilder) to set their own configurations to the input LoggerBuilder
 type LoggerConfig interface {
-	Apply(*LoggerBuilder)
+	Apply(lb *LoggerBuilder)
 }
 
 type multiconf struct {
 	confs []LoggerConfig
 }
 
+// MultiConf function is a wrapper for multiple configs to be bundled (and executed) in one shot.
+//
+// Similar to io.MultiWriter, it will iterate through all set LoggerConfig and run the same method
+// on each of them.
 func MultiConf(conf ...LoggerConfig) LoggerConfig {
 	allConf := make([]LoggerConfig, 0, len(conf))
 	allConf = append(allConf, conf...)
@@ -20,6 +31,8 @@ func MultiConf(conf ...LoggerConfig) LoggerConfig {
 	return &multiconf{allConf}
 }
 
+// Apply method will make a multiconf-type of LoggerConfig iterate through all its objects and
+// run the Apply method on the input pointer to a LoggerBuilder
 func (m multiconf) Apply(lb *LoggerBuilder) {
 	for _, c := range m.confs {
 		c.Apply(lb)
@@ -34,6 +47,8 @@ var defaultConfig LoggerConfig = &multiconf{
 	},
 }
 
+// LoggerConfigs is a map of LoggerConfig indexed by an int value. This is done in a map
+// and not a list for manual ordering, spacing and manipulation of preset entries
 var LoggerConfigs = map[int]LoggerConfig{
 	0: defaultConfig,
 	5: TextFormat,
@@ -43,32 +58,37 @@ var LoggerConfigs = map[int]LoggerConfig{
 }
 
 var (
-	DefaultCfg   LoggerConfig = LoggerConfigs[0]
-	TextCfg      LoggerConfig = LoggerConfigs[5]
-	JSONCfg      LoggerConfig = LoggerConfigs[6]
-	StdOutCfg    LoggerConfig = LoggerConfigs[7]
-	DefPrefixCfg LoggerConfig = LoggerConfigs[8]
+	DefaultCfg   LoggerConfig = LoggerConfigs[0] // placeholder for an initialized default LoggerConfig
+	TextCfg      LoggerConfig = LoggerConfigs[5] // placeholder for an initialized Text-LogFormatter LoggerConfig
+	JSONCfg      LoggerConfig = LoggerConfigs[6] // placeholder for an initialized JSON-LogFormatter LoggerConfig
+	StdOutCfg    LoggerConfig = LoggerConfigs[7] // placeholder for an initialized os.Stdout LoggerConfig
+	DefPrefixCfg LoggerConfig = LoggerConfigs[8] // placeholder for an initialized default-prefix LoggerConfig
 )
 
+// LCPrefix struct is a custom LoggerConfig to define prefixes to new Loggers
 type LCPrefix struct {
 	p string
 }
 
-func WithPrefix(prefix string) *LCPrefix {
+// WithPrefix function will allow creating a LoggerConfig that applies a prefix string to a Logger
+func WithPrefix(prefix string) LoggerConfig {
 	return &LCPrefix{
 		p: prefix,
 	}
 }
 
+// Apply method will set the configured prefix string to the input pointer to a LoggerBuilder
 func (c *LCPrefix) Apply(lb *LoggerBuilder) {
 	lb.prefix = c.p
 }
 
+// LCOut struct is a custom LoggerConfig to define the output io.Writer to new Loggers
 type LCOut struct {
 	out io.Writer
 }
 
-func WithOut(out ...io.Writer) *LCOut {
+// WithOut function will allow creating a LoggerConfig that applies a (number of) io.Writer to a Logger
+func WithOut(out ...io.Writer) LoggerConfig {
 	if len(out) == 0 {
 		return &LCOut{
 			out: os.Stdout,
@@ -93,6 +113,7 @@ func WithOut(out ...io.Writer) *LCOut {
 	}
 }
 
+// Apply method will set the configured output io.Writer to the input pointer to a LoggerBuilder
 func (c *LCOut) Apply(lb *LoggerBuilder) {
 	lb.out = c.out
 }

b7f282e461da5c15066828aa92823c658e379788
ZalgoNoise <zalgo.noise@gmail.com>
2022-03-19 19:13:37 +0100

    new: adding a Comm channel and func to LogServer

--- proto/message/message.go
+++ proto/message/message.go
@@ -4,18 +4,41 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"regexp"
+	"time"
+
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+)
+
+var (
+	contextCancelledRegexp = regexp.MustCompile(`rpc error: code = Canceled desc = context canceled`)
 )
 
 type LogServer struct {
 	MsgCh chan *MessageRequest
 	Done  chan struct{}
 	ErrCh chan error
+	Comm  chan *MessageRequest
+}
+
+func newComm(level int32, method, message string) *MessageRequest {
+	return &MessageRequest{
+		Time:   timestamppb.New(time.Now()),
+		Prefix: "gRPC",
+		Sub:    method,
+		Level:  level,
+		Msg:    message,
+	}
 }
 
 func (s *LogServer) Log(ctx context.Context, in *MessageRequest) (*MessageResponse, error) {
-	// fmt.Print(CtxGet(ctx), "::")
+
+	reqID := CtxGet(ctx)
+	s.Comm <- newComm(1, "Log", fmt.Sprintf("recv: %s", reqID))
+
 	s.MsgCh <- in
 
+	s.Comm <- newComm(1, "Log", fmt.Sprintf("send: %s", reqID))
 	return &MessageResponse{Ok: true}, nil
 }
 
@@ -23,6 +46,7 @@ func NewLogServer() *LogServer {
 	return &LogServer{
 		MsgCh: make(chan *MessageRequest),
 		Done:  make(chan struct{}),
+		Comm:  make(chan *MessageRequest),
 	}
 }
 
@@ -30,19 +54,33 @@ func (s *LogServer) LogStream(stream LogService_LogStreamServer) error {
 	go func() {
 		for {
 			in, err := stream.Recv()
+			ctx := stream.Context()
+			reqID := CtxGet(ctx)
+
 			if err == io.EOF {
+				s.Comm <- newComm(1, "LogStream", fmt.Sprintf("recv: got EOF from %s", reqID))
 				break
 			}
 			if err != nil {
+				if contextCancelledRegexp.MatchString(err.Error()) {
+					s.Comm <- newComm(2, "LogStream", fmt.Sprintf("recv: got context closure from %s :: %s", reqID, err))
+					return
+				}
+
+				s.Comm <- newComm(4, "LogStream", fmt.Sprintf("recv: got error from %s :: %s", reqID, err))
+
 				s.ErrCh <- err
 				return
 			}
-			// ctx := stream.Context()
-			// fmt.Print(CtxGet(ctx), "::")
+
+			s.Comm <- newComm(1, "LogStream", fmt.Sprintf("recv: %s", reqID))
 
 			s.MsgCh <- in
+
+			s.Comm <- newComm(1, "LogStream", fmt.Sprintf("send: %s", reqID))
 			err = stream.Send(&MessageResponse{Ok: true})
 			if err != nil {
+				s.Comm <- newComm(4, "LogStream", fmt.Sprintf("send: got error with %s :: %s", reqID, err))
 				s.ErrCh <- err
 				return
 			}
@@ -51,6 +89,6 @@ func (s *LogServer) LogStream(stream LogService_LogStreamServer) error {
 
 	for {
 		err := <-s.ErrCh
-		fmt.Println(err)
+		return err
 	}
 }

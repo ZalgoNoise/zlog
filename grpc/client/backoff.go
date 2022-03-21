81635ece227d4a14c5b9f738038d4604a618a6ab
ZalgoNoise <zalgo.noise@gmail.com>
2022-03-21 19:07:52 +0100

    fix: re-ordered methods in backoff.go

--- grpc/client/backoff.go
+++ grpc/client/backoff.go
@@ -38,47 +38,6 @@ func NewBackoff() *ExpBackoff {
 	return b
 }
 
-func (b *ExpBackoff) Counter() int {
-	return int(b.counter)
-}
-func (b *ExpBackoff) Max() string {
-	return b.max.String()
-}
-func (b *ExpBackoff) Current() string {
-	return b.wait.String()
-}
-
-func (b *ExpBackoff) WithDone(done *chan struct{}) *ExpBackoff {
-	b.exit = done
-	return b
-}
-
-func (b *ExpBackoff) Time(t time.Duration) *ExpBackoff {
-	b.max = t
-	return b
-}
-
-func (b *ExpBackoff) RegisterStream(call streamFunc, errCh chan error) *ExpBackoff {
-	b.call = call
-	b.errCh = errCh
-	return b
-}
-
-func (b *ExpBackoff) RegisterLog(call logFunc, errCh chan error) *ExpBackoff {
-	b.call = call
-	b.errCh = errCh
-	return b
-}
-
-func (b *ExpBackoff) AddMessage(msg *log.LogMessage) *ExpBackoff {
-	b.msg = append(b.msg, msg)
-	return b
-}
-
-// func (b *ExpBackoff) flush() {
-// 	b.msg = []*log.LogMessage{}
-// }
-
 func (b *ExpBackoff) Increment() *ExpBackoff {
 	if b.locked {
 		return b
@@ -89,18 +48,6 @@ func (b *ExpBackoff) Increment() *ExpBackoff {
 			rand.NewSource(time.Now().UnixNano())).Int63n(1000),
 	)
 	return b
-}
-
-func (b *ExpBackoff) Lock() {
-	b.locked = true
-}
-
-func (b *ExpBackoff) Unlock() {
-	b.locked = false
-}
-
-func (b *ExpBackoff) IsLocked() bool {
-	return b.locked
 }
 
 func (b *ExpBackoff) Wait() (func(), error) {
@@ -131,4 +78,59 @@ func (b *ExpBackoff) Wait() (func(), error) {
 
 	return nil, ErrFailedRetry
 
+}
+
+func (b *ExpBackoff) RegisterStream(call streamFunc, errCh chan error) *ExpBackoff {
+	b.call = call
+	b.errCh = errCh
+	return b
+}
+
+func (b *ExpBackoff) RegisterLog(call logFunc, errCh chan error) *ExpBackoff {
+	b.call = call
+	b.errCh = errCh
+	return b
+}
+
+func (b *ExpBackoff) WithDone(done *chan struct{}) *ExpBackoff {
+	b.exit = done
+	return b
+}
+
+func (b *ExpBackoff) Time(t time.Duration) *ExpBackoff {
+	b.max = t
+	return b
+}
+
+func (b *ExpBackoff) AddMessage(msg *log.LogMessage) *ExpBackoff {
+	b.msg = append(b.msg, msg)
+	return b
+}
+
+// func (b *ExpBackoff) flush() {
+// 	b.msg = []*log.LogMessage{}
+// }
+
+func (b *ExpBackoff) Counter() int {
+	return int(b.counter)
+}
+
+func (b *ExpBackoff) Max() string {
+	return b.max.String()
+}
+
+func (b *ExpBackoff) Current() string {
+	return b.wait.String()
+}
+
+func (b *ExpBackoff) Lock() {
+	b.locked = true
+}
+
+func (b *ExpBackoff) Unlock() {
+	b.locked = false
+}
+
+func (b *ExpBackoff) IsLocked() bool {
+	return b.locked
 }

70cfc111b1bdc39aad671d1fe5ac00a50c39d110
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-11 19:05:39 +0200

    fix: added code comments

--- store/sql/sqlite/sqlite.go
+++ store/sql/sqlite/sqlite.go
@@ -22,12 +22,15 @@ var (
 	ErrDBNotSetUp error = errors.New("database is not setup -- nil pointer")
 )
 
+// SQLite3 struct is a wrapper for a SQLite database to be used as a Log Writer
 type SQLite3 struct {
 	path  string
 	table string
 	db    *sql.DB
 }
 
+// New function will take in a path to a .db file, and a table name; and create a new
+// instance of a SQLite3 object; returning a pointer to one and an error.
 func New(path, table string) (*SQLite3, error) {
 
 	db, err := sql.Open("sqlite3", path)
@@ -55,6 +58,9 @@ func New(path, table string) (*SQLite3, error) {
 
 }
 
+// Load function will take in a path to a .db file, and a table name; and create a new
+// instance of a SQLite3 object based on an existing database; returning a pointer to
+// one and an error.
 func Load(path, table string) (*SQLite3, error) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
@@ -73,6 +79,8 @@ func Load(path, table string) (*SQLite3, error) {
 	}, nil
 }
 
+// Insert method will register any number of LogMessages in the SQLite database, returning
+// an error
 func (s *SQLite3) Insert(msg ...*log.LogMessage) error {
 
 	if len(msg) == 0 {
@@ -146,11 +154,14 @@ func (s *SQLite3) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// LCSQLite struct defines the Logger Config object that adds a SQLite writer to a Logger
 type LCSQLite struct {
 	out io.Writer
 	fmt log.LogFormatter
 }
 
+// WithSQLite function takes in a path to a .db file, and a table name; and returns a LoggerConfig
+// so that this type of writer is defined in a Logger
 func WithSQLite(path, table string) log.LoggerConfig {
 	var db = &SQLite3{}
 	var lerr, nerr error
@@ -159,7 +170,7 @@ func WithSQLite(path, table string) log.LoggerConfig {
 	if lerr != nil {
 		db, nerr = New(path, table)
 		if nerr != nil {
-			fmt.Errorf("failed to open or create database with errors; open: %s ; create: %s", lerr, nerr)
+			fmt.Printf("failed to open or create database with errors; open: %s ; create: %s", lerr, nerr)
 			os.Exit(1)
 		}
 	}
@@ -170,6 +181,7 @@ func WithSQLite(path, table string) log.LoggerConfig {
 	}
 }
 
+// Apply method will set the input LoggerBuilder's outputs and format to the LCSQLite object's.
 func (c *LCSQLite) Apply(lb *log.LoggerBuilder) {
 	lb.Out = c.out
 	lb.Fmt = c.fmt

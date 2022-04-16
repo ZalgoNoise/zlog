300b093f4931d27e8da2ec8dbd3d3178a3cb043d
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-16 17:32:42 +0200

    new: documented nilWriter's code

--- store/nilwritter.go
+++ store/nilwritter.go
@@ -2,10 +2,14 @@ package store
 
 import "io"
 
+// EmptyWriter is an exported, initialized nilWriter
 var EmptyWriter io.Writer = nilWritter{}
 
 type nilWritter struct{}
 
+// Write method implements the io.Writer interface, which in this case just returns
+// 0, nil -- as any writes to this writer will be discarded and are not perceived as
+// errors
 func (nilWritter) Write(p []byte) (n int, err error) {
 	return 0, nil
 }

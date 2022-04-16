bb82325d6222065a5ff765bff2cdcaa962db810b
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-16 17:52:21 +0200

    fix: separate message model and DBWriter interface

---
+++ store/db/message/model.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/zalgonoise/zlog/log"
+	"gorm.io/gorm"
+)
+
+type LogMessage struct {
+	gorm.Model
+	Time     time.Time
+	Prefix   string
+	Sub      string
+	Level    string
+	Msg      string
+	Metadata string
+}
+
+func (m *LogMessage) From(msg *log.LogMessage) error {
+	m.Time = msg.Time
+	m.Prefix = msg.Prefix
+	m.Sub = msg.Sub
+	m.Level = msg.Level
+	m.Msg = msg.Msg
+
+	meta, err := json.Marshal(msg.Metadata)
+
+	if err != nil {
+		return err
+	}
+
+	if metafmt := string(meta); metafmt != "{}" {
+		m.Metadata = metafmt
+	}
+
+	return nil
+}

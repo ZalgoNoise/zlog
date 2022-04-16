223e3b7be750b3b95cbfc625e0b16b3b46162df2
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-16 18:03:43 +0200

    fix: reduced DBWriter scope to an io.WriteCloser

--- store/db/db.go
+++ store/db/db.go
@@ -2,12 +2,8 @@ package db
 
 import (
 	"io"
-
-	"github.com/zalgonoise/zlog/log"
 )
 
 type DBWriter interface {
-	io.Writer
-	Create(msg ...*log.LogMessage) error
-	Close() error
+	io.WriteCloser
 }

216710aea3b47a83f7f92f403b0227432c879d80
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-17 17:45:43 +0200

    new: bson formatter pkg

---
+++ log/format/bson/bson.go
@@ -0,0 +1,22 @@
+package bson
+
+import (
+	"github.com/zalgonoise/zlog/log/event"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// FmtBSON struct describes the different manipulations and processing that a BSON LogFormatter
+// can apply to a LogMessage
+type FmtBSON struct{}
+
+// Format method will take in a pointer to a LogMessage; and returns a buffer and an error.
+//
+// This method will process the input LogMessage and marshal it according to this LogFormatter
+func (f *FmtBSON) Format(log *event.Event) (buf []byte, err error) {
+	// remove trailing newline on JSON format
+	if log.Msg[len(log.Msg)-1] == 10 {
+		log.Msg = log.Msg[:len(log.Msg)-1]
+	}
+
+	return bson.Marshal(log)
+}

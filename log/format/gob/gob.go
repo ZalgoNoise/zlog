99f6627cfbf75aaa8291a580d658698a31e6f87e
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-19 16:40:14 +0200

    fix: adapted gob encoding to protobuf event

--- log/format/gob/gob.go
+++ log/format/gob/gob.go
@@ -3,9 +3,9 @@ package gob
 import (
 	"bytes"
 	"encoding/gob"
+	"time"
 
 	"github.com/zalgonoise/zlog/log/event"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // FmtGob struct allows marshalling a LogMessage as gob-encoded bytes
@@ -15,15 +15,32 @@ type FmtGob struct{}
 // encode() method; which converts it to gob-encoded bytes, returning a slice of bytes and an
 // error
 func (f *FmtGob) Format(log *event.Event) ([]byte, error) {
+	type Event struct {
+		Time   time.Time
+		Prefix string
+		Sub    string
+		Level  string
+		Msg    string
+		Meta   map[string]interface{}
+	}
+
 	buf := &bytes.Buffer{}
-	gob.Register(&timestamppb.Timestamp{})
-	gob.Register(event.Level(0))
-	gob.Register(event.Field{})
+
+	gob.Register(Event{})
 	gob.Register(map[string]interface{}{})
+
+	e := Event{
+		Time:   log.GetTime().AsTime(),
+		Prefix: log.GetPrefix(),
+		Sub:    log.GetSub(),
+		Level:  log.GetLevel().String(),
+		Msg:    log.GetMsg(),
+		Meta:   log.Meta.AsMap(),
+	}
 
 	enc := gob.NewEncoder(buf)
 
-	err := enc.Encode(log)
+	err := enc.Encode(e)
 
 	return buf.Bytes(), err
 

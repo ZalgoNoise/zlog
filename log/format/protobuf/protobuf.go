a2bb6927091c6696aa860dcfb01bf2a90d04e335
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-19 16:40:36 +0200

    new: added protobuf encoding to protobuf event

---
+++ log/format/protobuf/protobuf.go
@@ -0,0 +1,18 @@
+package protobuf
+
+import (
+	"github.com/zalgonoise/zlog/log/event"
+	"google.golang.org/protobuf/proto"
+)
+
+// FmtPB struct is a placeholder LogFormatter to seamlessly convert protobuf messages into
+// byte slices for data transmission
+type FmtPB struct{}
+
+// Format method will take in a pointer to an Event; and returns a slice of bytes
+// and an error.
+//
+// This method will process the input Event and marshal it according to this LogFormatter
+func (f *FmtPB) Format(log *event.Event) (buf []byte, err error) {
+	return proto.Marshal(log)
+}

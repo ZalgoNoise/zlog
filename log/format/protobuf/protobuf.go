4636aa34fa9884cd8ca589790e829e571ae4a419
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-22 19:26:07 +0200

    fix: updated protobuf docs

--- log/format/protobuf/protobuf.go
+++ log/format/protobuf/protobuf.go
@@ -9,10 +9,10 @@ import (
 // byte slices for data transmission
 type FmtPB struct{}
 
-// Format method will take in a pointer to an Event; and returns a slice of bytes
+// Format method will take in a pointer to an event.Event; and returns a slice of bytes
 // and an error.
 //
-// This method will process the input Event and marshal it according to this LogFormatter
+// This method will process the input event.Event and marshal it according to this LogFormatter
 func (f *FmtPB) Format(log *event.Event) (buf []byte, err error) {
 	return proto.Marshal(log)
 }

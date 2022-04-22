62f8ccc3dbdc7cb5fc3860b5f0e3e7ea0823ba20
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-22 19:25:10 +0200

    fix: updated gob docs

--- log/format/gob/gob.go
+++ log/format/gob/gob.go
@@ -8,10 +8,10 @@ import (
 	"github.com/zalgonoise/zlog/log/event"
 )
 
-// FmtGob struct allows marshalling a LogMessage as gob-encoded bytes
+// FmtGob struct allows marshalling an event.Event as gob-encoded bytes
 type FmtGob struct{}
 
-// Format method will take in a pointer to a LogMessage, and return the execution of its
+// Format method will take in a pointer to an event.Event, and return the execution of its
 // encode() method; which converts it to gob-encoded bytes, returning a slice of bytes and an
 // error
 func (f *FmtGob) Format(log *event.Event) ([]byte, error) {

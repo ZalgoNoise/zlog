082802210a27d7afdfcdb8e7b9923b73b5c07b13
ZalgoNoise <zalgo.noise@gmail.com>
2022-04-28 19:00:57 +0200

    fix: updated Gob with its own type and decoder

--- log/format/json/json.go
+++ log/format/json/json.go
@@ -5,11 +5,43 @@ import (
 	"time"
 
 	"github.com/zalgonoise/zlog/log/event"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // FmtJSON struct describes the different manipulations and processing that a JSON LogFormatter
 // can apply to an event.Event
 type FmtJSON struct{}
+
+type entry struct {
+	Time   time.Time              `json:"timestamp,omitempty"`
+	Prefix string                 `json:"service,omitempty"`
+	Sub    string                 `json:"module,omitempty"`
+	Level  string                 `json:"level,omitempty"`
+	Msg    string                 `json:"message,omitempty"`
+	Meta   map[string]interface{} `json:"metadata,omitempty"`
+}
+
+func Decode(b []byte) (*event.Event, error) {
+	e := new(entry)
+
+	err := json.Unmarshal(b, e)
+
+	if err != nil {
+		return nil, err
+	}
+
+	log := event.New().
+		Prefix(e.Prefix).
+		Sub(e.Sub).
+		Level(event.Level(event.Level_value[e.Level])).
+		Message(e.Msg).
+		Metadata(e.Meta).
+		Build()
+
+	log.Time = timestamppb.New(e.Time)
+
+	return log, nil
+}
 
 // Format method will take in a pointer to an event.Event; and returns a buffer and an error.
 //
@@ -20,16 +52,7 @@ func (f *FmtJSON) Format(log *event.Event) (buf []byte, err error) {
 		*log.Msg = log.GetMsg()[:len(log.GetMsg())-1]
 	}
 
-	type Event struct {
-		Time   time.Time              `json:"timestamp,omitempty"`
-		Prefix string                 `json:"service,omitempty"`
-		Sub    string                 `json:"module,omitempty"`
-		Level  string                 `json:"level,omitempty"`
-		Msg    string                 `json:"message,omitempty"`
-		Meta   map[string]interface{} `json:"metadata,omitempty"`
-	}
-
-	return json.Marshal(Event{
+	return json.Marshal(entry{
 		Time:   log.GetTime().AsTime(),
 		Prefix: log.GetPrefix(),
 		Sub:    log.GetSub(),

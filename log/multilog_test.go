9008a1a9eeb13dafe377997ee84598b1f68b86dc
ZalgoNoise <zalgo.noise@gmail.com>
2022-05-04 19:58:45 +0200

    new: added more multiLogger tests

--- log/multilog_test.go
+++ log/multilog_test.go
@@ -212,11 +212,7 @@ func TestMultiLoggerSetOuts(t *testing.T) {
 	var verifyLogger = func(l *logger, tw io.Writer) (pass bool) {
 		out := l.out
 
-		if tw == nil && !reflect.DeepEqual(out, tw) {
-			pass = true
-		}
-
-		if tw != nil && !reflect.DeepEqual(out, tw) {
+		if !reflect.DeepEqual(out, tw) {
 			pass = true
 		}
 
@@ -286,6 +282,362 @@ func TestMultiLoggerSetOuts(t *testing.T) {
 		verify(idx, test)
 	}
 
+}
+
+func TestMultiLoggerAddOuts(t *testing.T) {
+	module := "MultiLogger"
+	funcname := "AddOuts()"
+
+	type test struct {
+		name string
+		w    []io.Writer
+	}
+
+	var fakeConnAddr = &connAddrTestLogger{}
+	var loggers = []Logger{
+		New(), New(), New(), New(),
+	}
+
+	var addr = address.New("test")
+
+	var tests = []test{
+		{
+			name: "empty config",
+			w:    []io.Writer{},
+		},
+		{
+			name: "nil config",
+			w:    nil,
+		},
+		{
+			name: "one writer",
+			w:    []io.Writer{os.Stdout},
+		},
+		{
+			name: "one ConnAddr",
+			w:    []io.Writer{addr},
+		},
+		{
+			name: "one nil writer",
+			w:    []io.Writer{nil},
+		},
+	}
+
+	var reset = func() {
+		for _, l := range loggers {
+			l.SetOuts(os.Stderr)
+		}
+		fakeConnAddr.outs = []io.Writer{}
+	}
+
+	var init = func(test test) *multiLogger {
+		ml := MultiLogger(loggers[0], fakeConnAddr)
+
+		ml.AddOuts(test.w...)
+
+		return ml.(*multiLogger)
+	}
+
+	var getTestWriter = func(test test) (tw []io.Writer, isAddr bool) {
+		if len(test.w) == 1 {
+			tw = append(tw, test.w...)
+		}
+
+		if len(tw) > 0 {
+			_, ok := tw[0].(*address.ConnAddr)
+
+			if ok {
+				isAddr = true
+			}
+		}
+		tw = append(tw, os.Stderr)
+
+		return
+	}
+
+	var verifyLogger = func(l *logger, tw io.Writer) (pass bool) {
+		out := l.out
+
+		if !reflect.DeepEqual(out, tw) {
+			pass = true
+		}
+
+		return pass
+	}
+
+	var verifyConnAddr = func(l *connAddrTestLogger, tw io.Writer) (pass bool) {
+		if len(l.outs) > 0 {
+			out := l.outs[0]
+
+			if tw != nil && out == tw {
+				pass = true
+			}
+		}
+		return pass
+	}
+
+	var scanMultiLogger = func(ml *multiLogger, tw []io.Writer, isAddr bool) (pass bool) {
+		for _, logu := range ml.loggers {
+			l, ok := logu.(*logger)
+
+			if ok && !isAddr {
+				pass = verifyLogger(l, tw[0])
+
+			} else if !ok && isAddr {
+				l, ok := logu.(*connAddrTestLogger)
+				if ok {
+					pass = verifyConnAddr(l, tw[0])
+				}
+			}
+		}
+
+		return
+	}
+
+	var verify = func(idx int, test test) {
+		reset()
+		defer reset()
+
+		ml := init(test)
+
+		var tw []io.Writer
+		var isAddr bool
+		var pass bool
+
+		tw, isAddr = getTestWriter(test)
+
+		pass = scanMultiLogger(ml, tw, isAddr)
+
+		if !pass {
+
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] test failed -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.name,
+			)
+			return
+		}
+	}
+
+	for idx, test := range tests {
+		verify(idx, test)
+	}
+}
+
+func FuzzMultiLoggerPrefix(f *testing.F) {
+	module := "MultiLogger"
+	funcname := "Prefix()"
+
+	ml := MultiLogger(loggers...)
+
+	f.Add("test-prefix")
+	f.Fuzz(func(t *testing.T, a string) {
+		ml.Prefix(a)
+
+		for idx, l := range ml.(*multiLogger).loggers {
+			if l.(*logger).prefix != a {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] fuzzed prefix mismatch: wanted %s ; got %s",
+					idx,
+					module,
+					funcname,
+					a,
+					l.(*logger).prefix,
+				)
+			}
+		}
+	})
+}
+
+func FuzzMultiLoggerSub(f *testing.F) {
+	module := "MultiLogger"
+	funcname := "Sub()"
+
+	ml := MultiLogger(loggers...)
+
+	f.Add("test-prefix")
+	f.Fuzz(func(t *testing.T, a string) {
+		ml.Sub(a)
+
+		for idx, l := range ml.(*multiLogger).loggers {
+			if l.(*logger).sub != a {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] fuzzed subprefix mismatch: wanted %s ; got %s",
+					idx,
+					module,
+					funcname,
+					a,
+					l.(*logger).sub,
+				)
+			}
+		}
+	})
+}
+
+func TestMultiLoggerFields(t *testing.T) {
+	module := "MultiLogger"
+	funcname := "Fields()"
+
+	type test struct {
+		name  string
+		init  map[string]interface{}
+		input map[string]interface{}
+		want  map[string]interface{}
+	}
+
+	var tests = []test{
+		{
+			name:  "default blank call",
+			init:  map[string]interface{}{},
+			input: map[string]interface{}{},
+			want:  map[string]interface{}{},
+		},
+		{
+			name:  "overwrite with blank",
+			init:  map[string]interface{}{"a": true},
+			input: map[string]interface{}{},
+			want:  map[string]interface{}{},
+		},
+		{
+			name:  "overwrite with nil",
+			init:  map[string]interface{}{"a": true},
+			input: nil,
+			want:  map[string]interface{}{},
+		},
+		{
+			name:  "write",
+			init:  map[string]interface{}{},
+			input: map[string]interface{}{"a": true},
+			want:  map[string]interface{}{"a": true},
+		},
+		{
+			name:  "overwrite",
+			init:  map[string]interface{}{"a": false},
+			input: map[string]interface{}{"a": true},
+			want:  map[string]interface{}{"a": true},
+		},
+	}
+
+	var reset = func(ml *multiLogger) {
+		for _, l := range ml.loggers {
+			l.Fields(nil)
+		}
+	}
+
+	var init = func(test test) *multiLogger {
+		ml := MultiLogger(loggers...)
+
+		ml.Fields(test.init)
+
+		return ml.(*multiLogger)
+	}
+
+	var verify = func(idx int, test test) {
+		ml := init(test)
+		defer reset(ml)
+
+		ml.Fields(test.input)
+
+		for _, l := range ml.loggers {
+			if !reflect.DeepEqual(l.(*logger).meta, test.want) {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
+					idx,
+					module,
+					funcname,
+					test.want,
+					l.(*logger).meta,
+					test.name,
+				)
+				return
+			}
+		}
+
+	}
+
+	for idx, test := range tests {
+		verify(idx, test)
+	}
+}
+
+func TestMultiLoggerSkipExit(t *testing.T) {
+	module := "MultiLogger"
+	funcname := "SkipExit()"
+
+	type test struct {
+		name  string
+		input []Logger
+		want  bool
+	}
+
+	var tests = []test{
+		{
+			name:  "none skip exit",
+			input: []Logger{New(), New()},
+			want:  false,
+		},
+		{
+			name:  "first is skip exit",
+			input: []Logger{New(), New(SkipExit)},
+			want:  false,
+		},
+		{
+			name:  "middle is skip exit",
+			input: []Logger{New(), New(SkipExit), New()},
+			want:  false,
+		},
+		{
+			name:  "last is skip exit",
+			input: []Logger{New(), New(SkipExit)},
+			want:  false,
+		},
+		{
+			name:  "middle is not skip exit",
+			input: []Logger{New(SkipExit), New(), New(SkipExit)},
+			want:  false,
+		},
+		{
+			name:  "all skip exit",
+			input: []Logger{New(SkipExit), New(SkipExit), New(SkipExit)},
+			want:  true,
+		},
+	}
+
+	var reset = func(ml *multiLogger) {
+		for _, l := range ml.loggers {
+			l.(*logger).skipExit = false
+		}
+	}
+
+	var init = func(test test) *multiLogger {
+		return MultiLogger(test.input...).(*multiLogger)
+	}
+
+	var verify = func(idx int, test test) {
+		ml := init(test)
+		defer reset(ml)
+
+		ok := ml.IsSkipExit()
+
+		if ok != test.want {
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.want,
+				ok,
+				test.name,
+			)
+			return
+		}
+	}
+
+	for idx, test := range tests {
+		verify(idx, test)
+	}
 }
 
 // import (

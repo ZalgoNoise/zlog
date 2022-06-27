a0894ddb10a81951d6cb66d8d7795e96628d3946
ZalgoNoise <zalgo.noise@gmail.com>
2022-06-27 17:45:21 +0200

    fix: retouched tests to focus on modules' logic

--- grpc/server/timing_test.go
+++ grpc/server/timing_test.go
@@ -1,77 +1,17 @@
 package server
 
 import (
-	"bytes"
 	"context"
 	"errors"
-	"fmt"
-	"io"
+	"reflect"
 	"testing"
-	"time"
 
-	"github.com/zalgonoise/zlog/grpc/client"
 	"github.com/zalgonoise/zlog/log"
 	"github.com/zalgonoise/zlog/log/event"
-	jsonfmt "github.com/zalgonoise/zlog/log/format/json"
 	pb "github.com/zalgonoise/zlog/proto/service"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
-
-var testErrUnexpected = errors.New("unexpected error")
-
-type testServerStream struct{}
-
-func (testServerStream) SetHeader(metadata.MD) error {
-	return nil
-}
-
-func (testServerStream) SendHeader(metadata.MD) error {
-	return nil
-}
-
-func (testServerStream) SetTrailer(metadata.MD) {}
-
-func (testServerStream) Context() context.Context {
-	return context.Background()
-}
-
-func (testServerStream) SendMsg(m interface{}) error {
-	msg, ok := m.(*pb.LogResponse)
-
-	if !ok {
-		return testErrUnexpected
-	}
-
-	if msg.GetReqID() == "000" {
-		return testErrUnexpected
-	}
-
-	return nil
-}
-
-func (testServerStream) RecvMsg(m interface{}) error {
-	msg, ok := m.(*event.Event)
-
-	if !ok {
-		return testErrUnexpected
-	}
-
-	if msg.GetLevel().String() == "error" {
-		return testErrUnexpected
-	}
-
-	if msg.GetMsg() == "deadline" {
-		return status.Error(codes.DeadlineExceeded, "")
-	}
-
-	if msg.GetMsg() == "EOF" {
-		return io.EOF
-	}
-
-	return nil
-}
 
 func TestUnaryServerTiming(t *testing.T) {
 	module := "LogServer Interceptors"
@@ -81,175 +21,134 @@ func TestUnaryServerTiming(t *testing.T) {
 	_ = funcname
 
 	type test struct {
-		name string
-		s    *GRPCLogServer
+		name   string
+		ok     bool
+		logger log.Logger
+		req    *event.Event
+		reply  *pb.LogResponse
 	}
 
-	var buf = []*bytes.Buffer{{}, {}, {}}
+	var sInfo = &grpc.UnaryServerInfo{
+		FullMethod: "test",
+	}
+
+	var errStr = "failed to write message"
+	var wBytes int32 = 100
+
+	var handler grpc.UnaryHandler = func(ctx context.Context, req interface{}) (interface{}, error) {
+		r, ok := req.(*event.Event)
+
+		if !ok {
+			return nil, errInvalidRequest
+		}
+
+		if r.GetMsg() == "fail" {
+			res := &pb.LogResponse{
+				Ok:    false,
+				Err:   &errStr,
+				ReqID: "test",
+			}
+
+			return res, errInvalidRequest
+		}
+
+		res := &pb.LogResponse{
+			Ok:    true,
+			ReqID: "test",
+			Bytes: &wBytes,
+		}
+
+		return res, nil
+	}
 
 	var tests = []test{
 		{
-			name: "unary server timing test",
-			s: New(
-				WithLogger(log.New(log.WithOut(buf[0]), log.SkipExit)),
-				WithServiceLogger(log.New(log.WithOut(buf[1]), log.CfgTextLevelFirst, log.SkipExit, log.CfgFormatJSON)),
-				WithAddr("127.0.0.1:9099"),
-				WithGRPCOpts(),
-				WithTiming(),
-			),
+			name:   "unary server timing test",
+			ok:     true,
+			logger: log.New(log.NilConfig),
+			req:    event.New().Message("null").Build(),
+			reply: &pb.LogResponse{
+				Ok:    true,
+				ReqID: "test",
+				Bytes: &wBytes,
+			},
+		},
+		{
+			name:   "failing unary server timing test",
+			ok:     false,
+			logger: log.New(log.NilConfig),
+			req:    event.New().Message("fail").Build(),
+			reply: &pb.LogResponse{
+				Ok:    false,
+				Err:   &errStr,
+				ReqID: "test",
+			},
 		},
 	}
 
-	var reset = func() {
-		for _, b := range buf {
-			b.Reset()
-		}
-	}
+	var verify = func(idx int, test test) {
+		fn := UnaryServerTiming(test.logger)
 
-	var stop = func(test test) {
-		test.s.Stop()
-		return
-	}
-
-	var initClient = func() (client.GRPCLogger, chan error) {
-		return client.New(
-			client.WithLogger(log.New(log.WithOut(buf[2]), log.SkipExit)),
-			client.WithAddr("127.0.0.1:9099"),
-			client.UnaryRPC(),
-			client.WithGRPCOpts(),
+		res, err := fn(
+			context.Background(),
+			test.req,
+			sInfo,
+			handler,
 		)
-	}
-
-	var bufferFilter = func(in []byte) (events []*event.Event, err error) {
-		// split lines
-		var line [][]byte
-		var buf []byte
-
-		for _, b := range in {
-			if b == 10 {
-				if len(buf) > 0 {
-					copy := buf
-					line = append(line, copy)
-					buf = []byte{}
-				}
-				continue
-			}
-			buf = append(buf, b)
-		}
-
-		if len(buf) > 0 {
-			copy := buf
-			line = append(line, copy)
-			buf = []byte{}
-		}
-
-		for _, l := range line {
-			e, err := jsonfmt.Decode(l)
-			if err != nil {
-				return nil, err
-			}
-			events = append(events, e)
-		}
-
-		return
-	}
-
-	var verifyServiceLogger = func(
-		idx int,
-		test test,
-		c client.GRPCLogger,
-		done chan struct{},
-	) {
-		c.Info("null")
-		time.Sleep(time.Millisecond * 1000)
-
-		var err error
-
-		events, err := bufferFilter(buf[1].Bytes())
 
 		if err != nil {
+			if test.ok {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] unexpected error: %v -- action: %s",
+					idx,
+					module,
+					funcname,
+					err,
+					test.name,
+				)
+				return
+			}
+
+			if !errors.Is(err, errInvalidRequest) {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] unexpected error: wanted %v ; got %v -- action: %s",
+					idx,
+					module,
+					funcname,
+					errInvalidRequest,
+					err,
+					test.name,
+				)
+				return
+			}
+
+			return
+		}
+
+		r, ok := res.(*pb.LogResponse)
+
+		if !ok {
 			t.Errorf(
-				"#%v -- FAILED -- [%s] [%s] error decoding events: %v -- action: %s",
+				"#%v -- FAILED -- [%s] [%s] response is not of type *pb.LogResponse -- action: %s",
 				idx,
 				module,
 				funcname,
-				err,
 				test.name,
 			)
 			return
 		}
 
-		for _, e := range events {
-
-			if e.GetSub() != "timer" {
-				continue
-			}
-
-			meta := e.GetMeta().AsMap()
-
-			mTime, ok := meta["time"]
-
-			if !ok {
-				err = fmt.Errorf(
-					"time metadata isn't available: %v",
-					meta,
-				)
-				test.s.ErrCh <- err
-				return
-			}
-
-			if mTime == "" {
-				err = fmt.Errorf(
-					"time value isn't set: %v",
-					mTime,
-				)
-				test.s.ErrCh <- err
-				return
-			}
-		}
-
-		done <- struct{}{}
-	}
-
-	var verify = func(idx int, test test) {
-		defer reset()
-		defer stop(test)
-
-		var done = make(chan struct{})
-
-		go test.s.Serve()
-		time.Sleep(time.Second)
-
-		c, clientErr := initClient()
-
-		go verifyServiceLogger(idx, test, c, done)
-
-		for {
-			select {
-			case err := <-clientErr:
-				t.Errorf(
-					"#%v -- FAILED -- [%s] [%s] unexpected client error: %v -- action: %s",
-					idx,
-					module,
-					funcname,
-					err,
-					test.name,
-				)
-				return
-
-			case err := <-test.s.ErrCh:
-				t.Errorf(
-					"#%v -- FAILED -- [%s] [%s] unexpected server error: %v -- action: %s",
-					idx,
-					module,
-					funcname,
-					err,
-					test.name,
-				)
-				return
-			case <-done:
-				return
-			}
+		if !reflect.DeepEqual(r, test.reply) {
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.reply,
+				r,
+				test.name,
+			)
+			return
 		}
 	}
 
@@ -266,175 +165,87 @@ func TestStreamServerTiming(t *testing.T) {
 	_ = funcname
 
 	type test struct {
-		name string
-		s    *GRPCLogServer
+		name   string
+		ok     bool
+		logger log.Logger
+		req    string
 	}
 
-	var buf = []*bytes.Buffer{{}, {}, {}}
+	var sInfo = &grpc.StreamServerInfo{
+		FullMethod:     "test",
+		IsClientStream: true,
+		IsServerStream: true,
+	}
+
+	var handler grpc.StreamHandler = func(srv interface{}, stream grpc.ServerStream) error {
+		r, ok := srv.(string)
+
+		if !ok {
+			return errInvalidRequest
+		}
+
+		if r == "fail" {
+			return errInvalidRequest
+		}
+
+		return nil
+	}
 
 	var tests = []test{
 		{
-			name: "unary server timing test",
-			s: New(
-				WithLogger(log.New(log.WithOut(buf[0]), log.SkipExit)),
-				WithServiceLogger(log.New(log.WithOut(buf[1]), log.CfgTextLevelFirst, log.SkipExit, log.CfgFormatJSON)),
-				WithAddr("127.0.0.1:9099"),
-				WithGRPCOpts(),
-				WithTiming(),
-			),
+			name:   "unary server timing test",
+			ok:     true,
+			logger: log.New(log.NilConfig),
+			req:    "ok",
+		},
+
+		{
+			name:   "failing unary timing logging test",
+			ok:     false,
+			logger: log.New(log.NilConfig),
+			req:    "fail",
 		},
 	}
 
-	var reset = func() {
-		for _, b := range buf {
-			b.Reset()
-		}
-	}
+	var verify = func(idx int, test test) {
+		fn := StreamServerTiming(test.logger)
 
-	var stop = func(test test) {
-		test.s.Stop()
-		return
-	}
-
-	var initClient = func() (client.GRPCLogger, chan error) {
-		return client.New(
-			client.WithLogger(log.New(log.WithOut(buf[2]), log.SkipExit)),
-			client.WithAddr("127.0.0.1:9099"),
-			client.StreamRPC(),
-			client.WithGRPCOpts(),
+		err := fn(
+			test.req,
+			&testServerStream{
+				ctx: context.Background(),
+			},
+			sInfo,
+			handler,
 		)
-	}
-
-	var bufferFilter = func(in []byte) (events []*event.Event, err error) {
-		// split lines
-		var line [][]byte
-		var buf []byte
-
-		for _, b := range in {
-			if b == 10 {
-				if len(buf) > 0 {
-					copy := buf
-					line = append(line, copy)
-					buf = []byte{}
-				}
-				continue
-			}
-			buf = append(buf, b)
-		}
-
-		if len(buf) > 0 {
-			copy := buf
-			line = append(line, copy)
-			buf = []byte{}
-		}
-
-		for _, l := range line {
-			e, err := jsonfmt.Decode(l)
-			if err != nil {
-				return nil, err
-			}
-			events = append(events, e)
-		}
-
-		return
-	}
-
-	var verifyServiceLogger = func(
-		idx int,
-		test test,
-		c client.GRPCLogger,
-		done chan struct{},
-	) {
-		c.Info("null")
-		time.Sleep(time.Millisecond * 1000)
-
-		var err error
-
-		events, err := bufferFilter(buf[1].Bytes())
 
 		if err != nil {
-			t.Errorf(
-				"#%v -- FAILED -- [%s] [%s] error decoding events: %v -- action: %s",
-				idx,
-				module,
-				funcname,
-				err,
-				test.name,
-			)
+			if test.ok {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] unexpected error: %v -- action: %s",
+					idx,
+					module,
+					funcname,
+					err,
+					test.name,
+				)
+				return
+			}
+
+			if !errors.Is(err, errInvalidRequest) {
+				t.Errorf(
+					"#%v -- FAILED -- [%s] [%s] unexpected error: wanted %v ; got %v -- action: %s",
+					idx,
+					module,
+					funcname,
+					errInvalidRequest,
+					err,
+					test.name,
+				)
+				return
+			}
+
 			return
-		}
-
-		for _, e := range events {
-
-			if e.GetSub() != "timer" {
-				continue
-			}
-
-			meta := e.GetMeta().AsMap()
-
-			mTime, ok := meta["time"]
-
-			if !ok {
-				err = fmt.Errorf(
-					"time metadata isn't available: %v",
-					meta,
-				)
-				test.s.ErrCh <- err
-				return
-			}
-
-			if mTime == "" {
-				err = fmt.Errorf(
-					"time value isn't set: %v",
-					mTime,
-				)
-				test.s.ErrCh <- err
-				return
-			}
-		}
-
-		done <- struct{}{}
-	}
-
-	var verify = func(idx int, test test) {
-		defer reset()
-		defer stop(test)
-
-		var done = make(chan struct{})
-
-		go test.s.Serve()
-		time.Sleep(time.Second)
-
-		c, clientErr := initClient()
-
-		go verifyServiceLogger(idx, test, c, done)
-
-		for {
-			select {
-			case err := <-clientErr:
-				t.Errorf(
-					"#%v -- FAILED -- [%s] [%s] unexpected client error: %v -- action: %s",
-					idx,
-					module,
-					funcname,
-					err,
-					test.name,
-				)
-				return
-
-			case err := <-test.s.ErrCh:
-				t.Errorf(
-					"#%v -- FAILED -- [%s] [%s] unexpected server error: %v -- action: %s",
-					idx,
-					module,
-					funcname,
-					err,
-					test.name,
-				)
-				return
-			case <-done:
-				return
-			}
 		}
 	}
 
@@ -457,7 +268,6 @@ func TestTimingSendMsg(t *testing.T) {
 		ok   bool
 	}
 
-	var buf = new(bytes.Buffer)
 	var bytesResponse = []int32{
 		203,
 		1008,
@@ -471,8 +281,8 @@ func TestTimingSendMsg(t *testing.T) {
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: &pb.LogResponse{
@@ -486,8 +296,8 @@ func TestTimingSendMsg(t *testing.T) {
 		{
 			name: "errored test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: &pb.LogResponse{
@@ -534,14 +344,12 @@ func TestTimingRecvMsg(t *testing.T) {
 		ok   bool
 	}
 
-	var buf = new(bytes.Buffer)
-
 	var tests = []test{
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m:  event.New().Message("null").Build(),
@@ -550,8 +358,8 @@ func TestTimingRecvMsg(t *testing.T) {
 		{
 			name: "errored test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: event.New().Level(event.Level_error).Message("null").Build(),
@@ -592,14 +400,12 @@ func TestTimingSetHeader(t *testing.T) {
 		m    metadata.MD
 	}
 
-	var buf = new(bytes.Buffer)
-
 	var tests = []test{
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: metadata.New(map[string]string{
@@ -618,6 +424,19 @@ func TestTimingSetHeader(t *testing.T) {
 				module,
 				funcname,
 				err,
+				test.name,
+			)
+			return
+		}
+
+		if !reflect.DeepEqual(test.t.stream.(*testServerStream).hMD, test.m) {
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.t.stream.(*testServerStream).hMD,
+				test.m,
 				test.name,
 			)
 			return
@@ -642,14 +461,25 @@ func TestTimingSendHeader(t *testing.T) {
 		m    metadata.MD
 	}
 
-	var buf = new(bytes.Buffer)
-
 	var tests = []test{
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
+				method: "testLog",
+			},
+			m: metadata.New(map[string]string{
+				"test": "header",
+			}),
+		},
+		{
+			name: "error test",
+			t: &timingStream{
+				stream: &testServerStream{
+					sentHeader: true,
+				},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: metadata.New(map[string]string{
@@ -661,12 +491,13 @@ func TestTimingSendHeader(t *testing.T) {
 	var verify = func(idx int, test test) {
 		err := test.t.SendHeader(test.m)
 
-		if err != nil {
+		if err != nil && !errors.Is(err, errSentHeader) {
 			t.Errorf(
-				"#%v -- FAILED -- [%s] [%s] unexpected error: %v -- action: %s",
+				"#%v -- FAILED -- [%s] [%s] error mismatch: wanted %v ; got %v -- action: %s",
 				idx,
 				module,
 				funcname,
+				errSentHeader,
 				err,
 				test.name,
 			)
@@ -692,14 +523,12 @@ func TestTimingSetTrailer(t *testing.T) {
 		m    metadata.MD
 	}
 
-	var buf = new(bytes.Buffer)
-
 	var tests = []test{
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 			m: metadata.New(map[string]string{
@@ -710,6 +539,19 @@ func TestTimingSetTrailer(t *testing.T) {
 
 	var verify = func(idx int, test test) {
 		test.t.SetTrailer(test.m)
+
+		if !reflect.DeepEqual(test.t.stream.(*testServerStream).tMD, test.m) {
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.t.stream.(*testServerStream).tMD,
+				test.m,
+				test.name,
+			)
+			return
+		}
 	}
 
 	for idx, test := range tests {
@@ -729,14 +571,16 @@ func TestTimingContext(t *testing.T) {
 		t    *timingStream
 	}
 
-	var buf = new(bytes.Buffer)
+	rootCtx := context.Background()
 
 	var tests = []test{
 		{
 			name: "working test",
 			t: &timingStream{
-				stream: testServerStream{},
-				logger: log.New(log.WithOut(buf), log.SkipExit),
+				stream: &testServerStream{
+					ctx: rootCtx,
+				},
+				logger: log.New(log.NilConfig),
 				method: "testLog",
 			},
 		},
@@ -745,12 +589,14 @@ func TestTimingContext(t *testing.T) {
 	var verify = func(idx int, test test) {
 		ctx := test.t.Context()
 
-		if ctx == nil {
+		if !reflect.DeepEqual(ctx, rootCtx) {
 			t.Errorf(
-				"#%v -- FAILED -- [%s] [%s] unexpected nil context -- action: %s",
+				"#%v -- FAILED -- [%s] [%s] output mismatch error: wanted %v ; got %v -- action: %s",
 				idx,
 				module,
 				funcname,
+				rootCtx,
+				ctx,
 				test.name,
 			)
 			return

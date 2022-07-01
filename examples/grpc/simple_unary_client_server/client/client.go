46554ffc9920f2d2e87bb99fe07ad1f512174ac5
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-01 18:24:53 +0200

    fix: moved to grpc examples ; TLS from env

---
+++ examples/grpc/simple_unary_client_server/client/client.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/zalgonoise/zlog/grpc/client"
+	"github.com/zalgonoise/zlog/log"
+	"github.com/zalgonoise/zlog/log/event"
+)
+
+func getEnv(env string) (val string, ok bool) {
+	v := os.Getenv(env)
+
+	if v == "" {
+		return v, false
+	}
+
+	return v, true
+}
+
+func getTLSConf() client.LogClientConfig {
+	var tlsConf client.LogClientConfig
+
+	var withCert bool
+	var withKey bool
+	var withCA bool
+
+	certPath, ok := getEnv("TLS_CLIENT_CERT")
+
+	if ok {
+		withCert = true
+	}
+
+	keyPath, ok := getEnv("TLS_CLIENT_KEY")
+
+	if ok {
+		withKey = true
+	}
+
+	caPath, ok := getEnv("TLS_CA_CERT")
+
+	if ok {
+		withCA = true
+	}
+
+	if withCA {
+		if withCert && withKey {
+			tlsConf = client.WithTLS(caPath, certPath, keyPath)
+		} else {
+			tlsConf = client.WithTLS(caPath)
+		}
+	}
+
+	return tlsConf
+}
+
+func main() {
+	logger := log.New(
+		log.WithFormat(log.TextColorLevelFirst),
+	)
+
+	grpcLogger, errCh := client.New(
+		client.WithAddr("127.0.0.1:9099"),
+		client.UnaryRPC(),
+		client.WithLogger(
+			logger,
+		),
+		client.WithGRPCOpts(),
+		getTLSConf(),
+	)
+	_, done := grpcLogger.Channels()
+
+	msg1 := event.New().Message("all the way").Build()
+
+	grpcLogger.Log(msg1)
+	grpcLogger.Log(msg1)
+	grpcLogger.Log(msg1)
+	grpcLogger.Log(msg1)
+
+	for i := 0; i < 10; i++ {
+		grpcLogger.Log(event.New().Level(event.Level_warn).Message(fmt.Sprintf("#%v", i)).Build())
+		time.Sleep(time.Millisecond * 50)
+	}
+
+	for {
+		select {
+		case err := <-errCh:
+			panic(err)
+		case <-done:
+			return
+		}
+	}
+}
---
+++ examples/grpc/simple_unary_client_server/server/server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+
+	"github.com/zalgonoise/zlog/log"
+
+	"github.com/zalgonoise/zlog/grpc/server"
+)
+
+func getEnv(env string) (val string, ok bool) {
+	v := os.Getenv(env)
+
+	if v == "" {
+		return v, false
+	}
+
+	return v, true
+}
+
+func getTLSConf() server.LogServerConfig {
+	var tlsConf server.LogServerConfig
+
+	var withCert bool
+	var withKey bool
+	var withCA bool
+
+	certPath, ok := getEnv("TLS_SERVER_CERT")
+
+	if ok {
+		withCert = true
+	}
+
+	keyPath, ok := getEnv("TLS_SERVER_KEY")
+
+	if ok {
+		withKey = true
+	}
+
+	caPath, ok := getEnv("TLS_CA_CERT")
+
+	if ok {
+		withCA = true
+	}
+
+	if withCert && withKey {
+		if withCA {
+			tlsConf = server.WithTLS(certPath, keyPath, caPath)
+		} else {
+			tlsConf = server.WithTLS(certPath, keyPath)
+		}
+	}
+
+	return tlsConf
+}
+
+func main() {
+
+	grpcLogger := server.New(
+		server.WithLogger(
+			log.New(
+				log.WithFormat(log.TextColorLevelFirstSpaced),
+			),
+		),
+		server.WithServiceLogger(
+			log.New(
+				log.WithFormat(log.TextColorLevelFirstSpaced),
+			),
+		),
+		server.WithAddr("127.0.0.1:9099"),
+		server.WithGRPCOpts(),
+		getTLSConf(),
+	)
+	grpcLogger.Serve()
+
+}

5730071bc094c6ee53d73acd555cf882444395e9
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-19 12:19:49 +0200

    new: added grpc nil server tests

---
+++ grpc/server/nilserver_test.go
@@ -0,0 +1,45 @@
+package server
+
+import "testing"
+
+func TestNilServer(t *testing.T) {
+	module := "GRPCLogServer"
+	funcname := "MultiLogger()"
+
+	_ = module
+	_ = funcname
+
+	type test struct {
+		name string
+	}
+
+	var tests = []test{
+		{
+			name: "creating a nil LogServer",
+		},
+	}
+
+	var verify = func(idx int, test test) {
+		s := NilServer()
+
+		if _, ok := s.(*nilLogServer); !ok {
+			t.Errorf(
+				"#%v -- FAILED -- [%s] [%s] call did not output an obj of type *nilLogServer -- action: %s",
+				idx,
+				module,
+				funcname,
+				test.name,
+			)
+		}
+
+		// test interface calls
+		if s != nil {
+			s.Serve()
+			s.Stop()
+		}
+	}
+
+	for idx, test := range tests {
+		verify(idx, test)
+	}
+}

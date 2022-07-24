12967839f990efdd6898235aba89a67d89eba3f7
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-24 12:45:19 +0200

    new: added Channels method to multilogger

--- grpc/server/multilog.go
+++ grpc/server/multilog.go
@@ -1,5 +1,7 @@
 package server
 
+import "github.com/zalgonoise/zlog/log/event"
+
 type multiLogger struct {
 	loggers []LogServer
 }
@@ -76,4 +78,64 @@ func (l *multiLogger) Stop() {
 		}
 		go l.loggers[i].Stop()
 	}
+}
+
+func (l *multiLogger) Channels() (logCh, logSvCh chan *event.Event, errCh chan error) {
+
+	// make output channels
+	logCh = make(chan *event.Event)
+	logSvCh = make(chan *event.Event)
+	errCh = make(chan error)
+
+	// make channel slices according to configured loggers' length
+	logChSet := make([]chan *event.Event, len(l.loggers))
+	logSvChSet := make([]chan *event.Event, len(l.loggers))
+	errChSet := make([]chan error, len(l.loggers))
+
+	// get the channels from each configured logger
+	for _, l := range l.loggers {
+		lCh, lSvCh, eCh := l.Channels()
+
+		logChSet = append(logChSet, lCh)
+		logSvChSet = append(logSvChSet, lSvCh)
+		errChSet = append(errChSet, eCh)
+	}
+
+	// kick off goroutine for log channel
+	// any message received on the returned channel will be fanned out to all loggers
+	go func(ch chan *event.Event, chSet []chan *event.Event) {
+		for {
+			select {
+			case msg := <-ch:
+				for _, c := range chSet {
+					c <- msg
+				}
+			}
+		}
+	}(logCh, logChSet)
+
+	// kick off goroutine for service logger channel
+	// any message received on the returned channel will be fanned out to all loggers
+	go func(ch chan *event.Event, chSet []chan *event.Event) {
+		for {
+			select {
+			case msg := <-ch:
+				for _, c := range chSet {
+					c <- msg
+				}
+			}
+		}
+	}(logSvCh, logSvChSet)
+
+	// kick off goroutine for error channel
+	// any message received on any channel will be pushed to the output one
+	for _, ch := range errChSet {
+		go func(in, out chan error) {
+			for err := range in {
+				errCh <- err
+			}
+		}(ch, errCh)
+	}
+
+	return logCh, logSvCh, errCh
 }

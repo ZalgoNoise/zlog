ff6cdfb96b415337514110643666594889d2ed7f
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-24 12:45:09 +0200

    new: added Channels method to nil logger

--- grpc/server/nilserver.go
+++ grpc/server/nilserver.go
@@ -1,9 +1,14 @@
 package server
 
+import "github.com/zalgonoise/zlog/log/event"
+
 type nilLogServer struct{}
 
 func (nilLogServer) Serve() {}
 func (nilLogServer) Stop()  {}
+func (nilLogServer) Channels() (logCh, logSvCh chan *event.Event, errCh chan error) {
+	return nil, nil, nil
+}
 
 // NilServer func creates a gRPC Log Server that doesn't do anything,
 // mostly for tests using the LogServer interface

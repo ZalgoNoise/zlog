c60df9d1ff931271e534be0355554e8560ae9f09
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-25 19:07:49 +0200

    new: nilLogServer - added code doc for all methods

--- grpc/server/nilserver.go
+++ grpc/server/nilserver.go
@@ -4,8 +4,13 @@ import "github.com/zalgonoise/zlog/log/event"
 
 type nilLogServer struct{}
 
+// Serve is the implementation of the `Serve()` method, from the LogServer interface
 func (nilLogServer) Serve() {}
-func (nilLogServer) Stop()  {}
+
+// Stop is the implementation of the `Stop()` method, from the LogServer interface
+func (nilLogServer) Stop() {}
+
+// Channels is the implementation of the `Channels()` method, from the LogServer interface
 func (nilLogServer) Channels() (logCh, logSvCh chan *event.Event, errCh chan error) {
 	return nil, nil, nil
 }

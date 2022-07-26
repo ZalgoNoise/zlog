da1229c33fed091f9f6eca85b0e181043dafa186
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-26 19:24:29 +0200

    fix: smaller example

--- examples/logger/simple_logger/simple_logger.go
+++ examples/logger/simple_logger/simple_logger.go
@@ -7,14 +7,10 @@ import (
 func main() {
 
 	// following the fmt.Print() signature, with the default standard logger
-	log.Print("this is the simplest approach to entering a log message")
-	log.Printf("and can include formatting: %v %v %s", 3.5, true, "string")
-	log.Println("which is similar to fmt.Print() method calls")
-
 	// with the same patterns, using methods for appropriate log levels
-	log.Tracef("this is a trace message: %v", 5000)
-	log.Info("this is an info message")
-	log.Errorln("this is an error message")
+	log.Print("this is the simplest approach to entering a log message")
+	log.Tracef("and can include formatting: %v %v %s", 3.5, true, "string")
+	log.Errorln("which is similar to fmt.Print() method calls")
 
 	// also, by default, log entries can cause runtime to stop
 	// such as log.Fatal() -- ends runtime with os.Exit(1)

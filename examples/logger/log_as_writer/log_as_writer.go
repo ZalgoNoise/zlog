1fb867eece15e4f74b3b557ea58abbec466d7421
ZalgoNoise <zalgo.noise@gmail.com>
2022-07-30 15:26:53 +0200

    fix: added error checking to example

--- examples/logger/log_as_writer/log_as_writer.go
+++ examples/logger/log_as_writer/log_as_writer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/zalgonoise/zlog/log"
 	"github.com/zalgonoise/zlog/log/event"
@@ -12,10 +13,18 @@ func main() {
 	logger := log.New()
 
 	n, err := logger.Write([]byte("Hello, world!"))
+	if err != nil {
+		fmt.Println("errored: ", err)
+		os.Exit(1)
+	}
 
 	fmt.Printf("\n---\nn: %v, err: %v\n---\n", n, err)
 
 	n, err = logger.Write(event.New().Message("Hi, world!").Build().Encode())
+	if err != nil {
+		fmt.Println("errored: ", err)
+		os.Exit(1)
+	}
 
 	fmt.Printf("\n---\nn: %v, err: %v\n---\n", n, err)
 }
